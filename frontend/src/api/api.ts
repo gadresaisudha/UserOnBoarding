b3f5b4ab7849d5e62e553f2ea362d77b7b4392b4
Sai Sudha <saisudhagadrewg542@gmail.com>
2025-08-06 19:42:13 -0500

    api frontend url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = process.env.BACKEND_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL+'/api',
   headers: {
     'Content-Type': 'application/json',
   },
